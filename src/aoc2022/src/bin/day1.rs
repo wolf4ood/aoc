df881cff94d00172e898dc349a547ce0f2d0e9d0
Enrico Risa <enrico.risa@gmail.com>
2022-12-01 21:32:03 +0100

    aoc2022#day1

---
+++ src/aoc2022/src/bin/day1.rs
@@ -0,0 +1,47 @@
+use std::collections::BTreeMap;
+
+use aoc_attributes::aoc_main;
+
+static INPUT: &str = include_str!("../../input/day1.txt");
+
+#[aoc_main(year = 2022, day = 1, part1 = "part1", part2 = "part2")]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    Ok(())
+}
+
+pub type ElfScore = BTreeMap<u32, Vec<u32>>;
+pub type ElfAccumulator = (ElfScore, u32, u32);
+
+pub fn input_generator(input: &str) -> Vec<i32> {
+    input.lines().map(|l| l.parse().unwrap()).collect()
+}
+
+pub fn part1() -> u32 {
+    groups().iter().rev().map(|item| item.0).take(1).sum()
+}
+pub fn part2() -> u32 {
+    groups()
+        .iter()
+        .rev()
+        .flat_map(|item| item.1.iter().map(move |_| item.0))
+        .take(3)
+        .sum()
+}
+
+fn groups() -> ElfScore {
+    INPUT
+        .lines()
+        .fold(ElfAccumulator::default(), |mut acc, item| {
+            if item.is_empty() {
+                let items = acc.0.entry(acc.1).or_insert_with(Vec::new);
+                items.push(acc.2);
+                acc.1 = 0;
+                acc.2 = acc.2 + 1;
+            } else {
+                let n: u32 = item.parse().unwrap();
+                acc.1 = acc.1 + n;
+            }
+            acc
+        })
+        .0
+}
