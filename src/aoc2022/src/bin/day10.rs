4be69cdc94fc592989a986abab6941ddb1a02065
Enrico Risa <enrico.risa@gmail.com>
2022-12-10 17:50:19 +0100

    aoc2022#day10 refactor

--- src/aoc2022/src/bin/day10.rs
+++ src/aoc2022/src/bin/day10.rs
@@ -1,7 +1,6 @@
 use std::{
     collections::{BTreeMap, VecDeque},
     fmt::Debug,
-    marker::PhantomData,
     str::FromStr,
 };
 
@@ -15,13 +14,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-pub struct Cpu<T> {
+pub struct Cpu {
     register: Register,
     ops: VecDeque<Instruction>,
     cycle: i32,
-    data: PhantomData<T>,
+    scheduler: Part1Scheduler,
 }
-impl<T> Debug for Cpu<T> {
+impl Debug for Cpu {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Cpu")
             .field("register", &self.register)
@@ -31,47 +30,61 @@ impl<T> Debug for Cpu<T> {
     }
 }
 
-pub struct Crt(Vec<char>);
+pub struct Crt([[char; 40]; 6]);
+
+impl Crt {
+    fn new() -> Self {
+        Crt([['.'; 40]; 6])
+    }
+
+    pub fn visible(&mut self, x: usize, y: usize) {
+        self.0[x][y] = '#';
+    }
+}
 
 impl Debug for Crt {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "CRT")?;
-        for mut chunk in &self.0.iter().chunks(40) {
-            writeln!(f, "{}", chunk.join(""))?;
+        for chunk in self.0.iter() {
+            writeln!(f, "{}", chunk.iter().join(""))?;
         }
         Ok(())
     }
 }
 
-impl Crt {
-    pub fn from_instructions(map: BTreeMap<i32, i32>) -> Self {
-        let mut vec = vec!['.'; map.len() - 1];
-        for rows in &map.iter().filter(|(k, _)| *k < &240).chunks(40) {
-            let mut col = 0;
-            for (k, v) in rows {
-                let idx = k - 1;
-                let range = v - 1..=v + 1;
-                vec[idx as usize] = range.contains(&col).then_some('#').unwrap_or('.');
-                col += 1;
-            }
-        }
-
-        Crt(vec)
-    }
-}
-
-impl<T: Scheduler> Cpu<T> {
+impl Cpu {
     pub fn new() -> Self {
         Self {
             register: Register(1),
             ops: VecDeque::new(),
             cycle: 1,
-            data: PhantomData,
+            scheduler: Part1Scheduler {},
         }
     }
 
     pub fn load(&mut self, op: Instruction) {
         self.schedule(op);
+    }
+
+    pub fn exec(
+        &mut self,
+        mut instructions: impl Iterator<Item = Instruction>,
+        mut cycle_hook: impl FnMut(i32, &Register),
+    ) {
+        {
+            while let Some(i) = instructions.next() {
+                self.schedule(i);
+
+                self.run_instructions(&mut cycle_hook);
+            }
+        }
+    }
+    fn run_instructions(&mut self, cycle_hook: &mut impl FnMut(i32, &Register)) {
+        while let Some(i) = self.ops.pop_front() {
+            cycle_hook(self.cycle, &self.register);
+            i.apply(&mut self.register);
+            self.cycle += 1;
+        }
     }
     pub fn run(&mut self) -> BTreeMap<i32, i32> {
         let mut map = BTreeMap::new();
@@ -85,20 +98,16 @@ impl<T: Scheduler> Cpu<T> {
     }
 
     fn schedule(&mut self, op: Instruction) {
-        for op in T::schedule(op) {
+        for op in self.scheduler.schedule(op) {
             self.ops.push_back(op);
         }
     }
 }
 
-pub trait Scheduler {
-    fn schedule(op: Instruction) -> Vec<Instruction>;
-}
-
 pub struct Part1Scheduler {}
 
-impl Scheduler for Part1Scheduler {
-    fn schedule(op: Instruction) -> Vec<Instruction> {
+impl Part1Scheduler {
+    fn schedule(&self, op: Instruction) -> Vec<Instruction> {
         match op {
             Instruction::Noop => vec![op],
             Instruction::Add(_) => vec![Instruction::Noop, op],
@@ -118,33 +127,36 @@ impl FromStr for Instruction {
     }
 }
 
-fn solve1(input: &str) -> i32 {
-    let mut cpu: Cpu<Part1Scheduler> = Cpu::new();
-
-    let filter = vec![20, 60, 100, 140, 180, 220];
-    for i in instructions(input) {
-        cpu.load(i);
-    }
-
-    let map = cpu.run();
-
-    map.iter()
-        .filter(|(k, _)| filter.contains(k))
-        .map(|(k, v)| (*k as i32) * v)
-        .sum()
-}
-
 pub fn part1() -> i32 {
     solve1(INPUT)
 }
-fn solve2(input: &str) -> Crt {
-    let mut cpu: Cpu<Part1Scheduler> = Cpu::new();
-    for i in instructions(input) {
-        cpu.load(i);
-    }
-    let map = cpu.run();
 
-    Crt::from_instructions(map)
+fn solve1(input: &str) -> i32 {
+    let mut cpu = Cpu::new();
+
+    let mut signal = 0;
+    cpu.exec(instructions(input), |cycle, register| {
+        if cycle % 40 == 20 && cycle <= 220 {
+            signal += cycle * register.0;
+        }
+    });
+
+    signal
+}
+
+fn solve2(input: &str) -> Crt {
+    let mut cpu = Cpu::new();
+    let mut crt = Crt::new();
+    cpu.exec(instructions(input), |cycle, register| {
+        let range = register.0 - 1..=register.0 + 1;
+        let r = (cycle - 1) / 40;
+        let c = (cycle - 1) % 40;
+        if range.contains(&c) {
+            crt.visible(r as usize, c as usize);
+        }
+    });
+
+    crt
 }
 pub fn part2() -> Crt {
     solve2(INPUT)
